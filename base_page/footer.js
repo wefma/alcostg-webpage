5eb50c853edbf978b334f5fd1e8afeccd53b4479
wefma <62638744+wefma@users.noreply.github.com>
2025-02-17 23:16:43 +0900

    リファクタリング系 (#7)
    * ヘッダ作成 (#1)
    
    * ヘッダ作成 (#4)
    
    * ヘッダ作成
    
    * タイトル変更
    
    * ごあいさつページの追加
    
    * ごあいさつページ誤字修正
    
    * 共通している部分を外部のJavaScriptファイルに吐き出した
    
    * 終焉のindentを修正

---
+++ base_page/footer.js
@@ -0,0 +1,18 @@
+document.write(
+    '<footer id="footer">' +
+        '<div id="info" class="grid">' +
+            '<div class="logo">' +
+                '<a href="index.html"><img src="images/logo.png" width="45" height="45" alt="Sample site"></a>' +
+            '</div>' +
+        '</div>' +
+
+        '<ul class="footnav">' +
+            '<li><a href="greeting.html">ごあいさつ</a></li>' +
+            '<li><a href="basic.html">基本攻略情報</a></li>' +
+            '<li><a href="1st.html">1面攻略</a></li>' +
+            '<li><a href="2nd.html">2面攻略</a></li>' +
+            '<li><a href="3rd.html">3面攻略</a></li>' +
+        '</ul>' +
+    '</footer>' +
+    '<address>Design by <a href="http://f-tpl.com" target="_blank" rel="nofollow">http://f-tpl.com</a></address>'
+)
