813dcafa72066f7e87097852de5bed1d98781ac3
wefma <62638744+wefma@users.noreply.github.com>
2025-02-18 23:55:11 +0900

    サイドのトピックス関連 (#9)
    * 青カクテルの金額を1499円としていたことの修正
    
    * 作る気もないのに何故かサイドバーを入れた
    
    * 何故かトップページにもサイドバーと同じリンク入れた
    
    * 何故かサイドバーのページを先に作った

---
+++ base_page/sidebar.js
@@ -0,0 +1,12 @@
+document.write(
+    '<aside id="sidebar">' +
+        '<h3 class="heading">トピックス</h3>' +
+        '<article>' +
+            '<ul class="list">' +
+                '<li><a href="story.html"><img src="images/story.jpg" width="42" height="42" alt="">今からでも遅くない！？黄昏酒場のストーリーをまるっと紹介！</a></li>' +
+                '<li><a href="charm.html"><img src="images/interview.jpg" width="42" height="42" alt="">800万円オーバーが語る、黄昏酒場の魅力とは…？</a></li>' +
+                '<li><a href="istouhou.html"><img src="images/th8.jpg" width="42" height="42" alt="">徹底討論！黄昏酒場は東方なのか？</a></li>' +
+            '</ul>' +
+        '</article>' +
+    '</aside>'
+)
